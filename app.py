8bd882a5cd51010c68b6e17e3c85441ae29a9bf2
Warm <1870654579@qq.com>
2020-02-24 20:24:23 +0800

    five commit

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
-from flask import render_template
+import os
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+# 配置
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
