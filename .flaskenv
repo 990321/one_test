24eeb3c3be534317d46e3da7ee24e9819436b53f
Warm <1870654579@qq.com>
2020-02-28 00:52:14 +0800

    代码优化

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
-FLASK_ENV = development
-FLASK_App = watchlist
+FLASK_ENV=development
+FLASK_App=watchlist
--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,11 @@
 import os
 from dotenv import load_dotenv
-from watchlist import app
-
 
 dotenv_path = os.path.join(os.path.dirname(__file__),'.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
+
+from watchlist import app
     
 
 
